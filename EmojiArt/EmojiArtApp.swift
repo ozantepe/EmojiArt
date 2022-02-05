3dffeba5ccbeb9f8d8b002d46e5275eda143d7cb
Ozan Tepe <ozantepe332@gmail.com>
2022-02-05 18:01:20 +0100

    Initial commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Ozan Tepe on 05.02.22.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
